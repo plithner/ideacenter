5529dbbc4380e903915a41d77f7ef4fac8589459
Peter Lithner <peter@appdata.biz>
2016-11-11 11:13:03 +0100

    Changed some more wordings

--- qa-lang/custom/qa-lang-emails.php
+++ qa-lang/custom/qa-lang-emails.php
@@ -21,14 +21,14 @@
 */
 
 	return array(
-		'a_commented_body' => "Your feedback on ^site_title has a new comment by ^c_handle:\n\n^open^c_content^close\n\nYour feedback was:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_commented_subject' => 'Your ^site_title feedback has a new comment',
+		'a_commented_body' => "Your contribution to ^site_title has a new comment by ^c_handle:\n\n^open^c_content^close\n\nYour contribution was:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
+		'a_commented_subject' => 'Your ^site_title contribution has a new comment',
 
-		'a_followed_body' => "Your feedback on ^site_title has a new related idea by ^q_handle:\n\n^open^q_title^close\n\nYour feedback was:\n\n^open^a_content^close\n\nClick below to feedback the new idea:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_followed_subject' => 'Your ^site_title feedback has a related idea',
+		'a_followed_body' => "Your contribution to ^site_title has a new related idea by ^q_handle:\n\n^open^q_title^close\n\nYour contribution was:\n\n^open^a_content^close\n\nClick below to contribution to the new idea:\n\n^url\n\nThank you,\n\n^site_title",
+		'a_followed_subject' => 'Your ^site_title contribution has a related idea',
 
-		'a_selected_body' => "Congratulations! Your feedback on ^site_title has been selected as the best by ^s_handle:\n\n^open^a_content^close\n\nThe idea was:\n\n^open^q_title^close\n\nClick below to see your feedback:\n\n^url\n\nThank you,\n\n^site_title",
-		'a_selected_subject' => 'Your ^site_title feedback has been selected!',
+		'a_selected_body' => "Congratulations! Your contribution to ^site_title has been selected as the best by ^s_handle:\n\n^open^a_content^close\n\nThe idea was:\n\n^open^q_title^close\n\nClick below to see your contribution:\n\n^url\n\nThank you,\n\n^site_title",
+		'a_selected_subject' => 'Your ^site_title contribution has been selected!',
 
 		'c_commented_body' => "A new comment by ^c_handle has been added after your comment on ^site_title:\n\n^open^c_content^close\n\nThe discussion is following:\n\n^open^c_context^close\n\nYou may respond by adding another comment:\n\n^url\n\nThank you,\n\n^site_title",
 		'c_commented_subject' => 'Your ^site_title comment has been added to',
@@ -37,7 +37,7 @@
 		'confirm_subject' => '^site_title - Email Address Confirmation',
 
 		'feedback_body' => "Comments:\n^message\n\nName:\n^name\n\nEmail:\n^email\n\nPrevious page:\n^previous\n\nUser:\n^url\n\nIP address:\n^ip\n\nBrowser:\n^browser",
-		'feedback_subject' => '^ feedback',
+		'feedback_subject' => '^ contribution',
 
 		'flagged_body' => "A post by ^p_handle has received ^flags:\n\n^open^p_context^close\n\nClick below to see the post:\n\n^url\n\n\nClick below to review all flagged posts:\n\n^a_url\n\n\nThank you,\n\n^site_title",
 		'flagged_subject' => '^site_title has a flagged post',
@@ -53,13 +53,13 @@
 		'private_message_reply' => "Click below to reply to ^f_handle by private message:\n\n^url\n\n",
 		'private_message_subject' => 'Message from ^f_handle on ^site_title',
 
-		'q_answered_body' => "Your idea on ^site_title has been contributed to by ^a_handle:\n\n^open^a_content^close\n\nYour idea was:\n\n^open^q_title^close\n\nIf you like this feedback, you may select it as the best:\n\n^url\n\nThank you,\n\n^site_title",
+		'q_answered_body' => "Your idea on ^site_title has been contributed to by ^a_handle:\n\n^open^a_content^close\n\nYour idea was:\n\n^open^q_title^close\n\nIf you like this contribution, you may select it as the best:\n\n^url\n\nThank you,\n\n^site_title",
 		'q_answered_subject' => 'Your ^site_title idea was contributed to',
 
 		'q_commented_body' => "Your idea on ^site_title has a new comment by ^c_handle:\n\n^open^c_content^close\n\nYour idea was:\n\n^open^c_context^close\n\nYou may respond by adding your own comment:\n\n^url\n\nThank you,\n\n^site_title",
 		'q_commented_subject' => 'Your ^site_title idea has a new comment',
 
-		'q_posted_body' => "A new idea has been submitted by ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nClick below to see the idea:\n\n^url\n\nThank you,\n\n^site_title",
+		'q_posted_body' => "A new idea has been shared by ^q_handle:\n\n^open^q_title\n\n^q_content^close\n\nClick below to see the idea:\n\n^url\n\nThank you,\n\n^site_title",
 		'q_posted_subject' => '^site_title has a new idea',
 
 		'remoderate_body' => "An edited post by ^p_handle requires your reapproval:\n\n^open^p_context^close\n\nClick below to approve or hide the edited post:\n\n^url\n\n\nClick below to review all queued posts:\n\n^a_url\n\n\nThank you,\n\n^site_title",
